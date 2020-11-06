ee3e1b2fef5f8b9cab2c398b214bcc9864d8874b
DeMarcus Kirby <tkd024@gmail.com>
2020-11-06 16:03:12 -0700

    remove btn from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,3 @@
 class HomeController < ApplicationController
   def index; end
-
-  def update
-    users = User.all
-
-    users.each do |user|
-      unless user.email == 'tkd024@gmail.com'
-        user.update(role: 0)
-      else
-        user.update(role: 1)
-      end
-    end
-    redirect_to '/'
-  end
 end
