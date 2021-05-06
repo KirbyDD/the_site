7a67e75c69c6af38c085dbd969a4273357c18e7a
DeMarcus Kirby <tkd024@gmail.com>
2021-05-06 16:44:00 -0600

    Added boards to index along with styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-User.create!(username: "Tron Kat", email: "tkd024@gmail.com", password: "yasha321", password_confirmation: "yasha321", role: 1)
+# User.destroy_all
+# User.create!(username: "Tron Kat", email: "tkd024@gmail.com", password: "yasha321", password_confirmation: "yasha321", role: 1)
+
+# Board.destroy_all
+Board.create!(abbre: "A", name: "Anime")
+Board.create!(abbre: "B", name: "Random")
+Board.create!(abbre: "BK", name: "Books")
+Board.create!(abbre: "CAR", name: "Western Animated Shows")
+Board.create!(abbre: "DEV", name: "Coding")
+Board.create!(abbre: "M", name: "Manga")
+Board.create!(abbre: "MTG", name: "Magic The Gathering")
+Board.create!(abbre: "P", name: "Pets")
+Board.create!(abbre: "POKE", name: "Pokemon")
+Board.create!(abbre: "S", name: "Sports")
+Board.create!(abbre: "T", name: "Travel")
+Board.create!(abbre: "VG", name: "Video Games")
